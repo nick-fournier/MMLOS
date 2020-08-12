fe0aa41ba18e7d06d608633e97a3dd9d4fee9fd0
nick-fournier <nichfournier@gmail.com>
2020-08-12 14:24:17 -0700

    Added function for saturation flow rate and v/c ratio

--- R/automobiles.R
+++ R/automobiles.R
@@ -53,3 +53,146 @@ auto.S_R <- function(link, control) {
 }
 
 
+#' Determine the adjusted saturation flow rate
+#' 
+#' @param link Data.table of link data.
+#' @param control String containing the boundary intersection control type
+#' (Signalized", "AWSC - Stop", "TWSC - Stop", "Uncontrolled", "Yield")
+#' @return Numeric value in mi/hr.
+#' @examples
+#' auto.satflow(link, int)
+#' @export
+auto.satflow <- function(link, int) {
+  
+  #Constants
+  #Opposite street dir
+  odir = switch(link$link_dir,
+                "NB" = "SB",
+                "SB" = "NB",
+                "EB" = "WB",
+                "WB" = "EB")
+  
+  s0 = 1900 #Baseline saturation flow rate (1750 outside of major metro area)
+  E_T = 2 #Car equivalent of trucks
+  N_m = 20 #Parking maneuver rate
+  E_R = 1.18 #Equivalent car for right turns
+  E_L = 1.05 #Equivalent car for left turns
+  v0 = int[traf_dir == odir, v_v] #Opposiding demand flow
+  
+  ##Adjustment factors
+  #Lane width
+  if(link$W_ol < 10) {
+    f_w = 0.96
+  } else if(link$W_ol >= 10 & link$W_ol <= 12.9) {
+    f_w = 1
+  } else if(link$W_ol > 12.9) {
+    f_w = 1.04
+  }
+  
+  #Heavy vehicles
+  f_HV = 100 / (100 + link$P_HV*(E_T - 1))
+  #Slope grade
+  f_g = 1 - (link$p_g/200)
+  #Parking
+  f_p = ((link$N_th - 0.1 - (18*N_m/3600))/link$N_th)
+  f_p = ifelse(f_p < 0.05, 0.05, f_p)
+  #bus blocking
+  f_bb = (link$N_th - (14.4*link$n_bus/3600))/link$N_th
+  f_bb = ifelse(f_bb < 0.05, 0.05, f_bb)
+  #Area of type
+  f_a = 0.9
+  #Lane utilization
+  f_LU = 1
+  #Right turns adjustment
+  f_RT = 1 / E_R
+  #Left turn adjustment
+  f_LT = 1 / E_L
+  
+  ### pedestrian adjustment factor for right turns
+  #ped flow rate
+  v_pedg = int[traf_dir == link$link_dir, v_p*(C/gped)]
+  v_pedg = ifelse(v_pedg > 5000, 5000, v_pedg)
+  
+  #ped occupancy
+  if(int[traf_dir == link$link_dir, v_p] <= 2000) {
+    OCC_pedg = v_pedg/2000
+  } else {
+    OCC_pedg = 0.4 + v_pedg/10000
+    OCC_pedg = ifelse(OCC_pedg > 0.9, 0.9, OCC_pedg)
+  }
+  
+  #bicycle flow rate
+  v_bicg = int[traf_dir == link$link_dir, v_b*(C/gped)]
+  v_bicg = ifelse(v_bicg > 1900, 1900, v_bicg)
+
+  #bike occupancy
+  OCC_bicg = 0.02 + v_bicg/2700
+  
+  #conflict zone occupancy
+  OCC_rR = int[traf_dir == link$link_dir, gped/g*OCC_pedg + OCC_bicg - gped/g*OCC_pedg*OCC_bicg]
+  
+  #Unoccupied time
+  A_RpbT = 1 - OCC_rR
+  
+  #Saturation flow rate adjustment factor for right turns
+  f_Rpb = A_RpbT
+  
+  #Ped occupancy after queue clears
+  #Effective permitted green time
+  g_p = int[traf_dir == link$link_dir, g - l]
+  g_p = ifelse(g_p < 0, 0, g_p)
+  
+  #Effective permitted green time unblocked by opposite queue
+  g_u = int[traf_dir == odir, g - l]
+  
+  #Opposing queue service time
+  g_q = g_p - g_u 
+  
+  #Ped occupancy after queue clears
+  if( g_p < int[traf_dir == link$link_dir, gped]) {
+    OCC_pedu = int[traf_dir == link$link_dir, OCC_pedg*(1 - 0.5*g_q/gped)]
+  } else {
+    OCC_pedu = 0
+  }
+  
+  #Determine relevant conflict zone occupancy
+  OCC_rL = int[traf_dir == link$link_dir, ((gped - g_q) / (g_p - g_q)) * (OCC_pedu)*exp(-5*v0/3600)]
+  
+  #Unoccupied time
+  A_LpbT = 1 - OCC_rL
+  
+  #Saturation flow rate adjustment factor for left turns
+  f_Lpb = A_LpbT
+  
+  #Adjusted saturation flow rate
+  s = s0*f_w*f_HV*f_g*f_p*f_bb*f_a*f_LU*f_LT*f_RT*f_Lpb*f_Rpb
+  
+  return(s)
+}
+
+
+#' Determine the volume to capacity ratio for critical intersection
+#' 
+#' @param link Data.table of link data.
+#' @param control String containing the boundary intersection control type
+#' (Signalized", "AWSC - Stop", "TWSC - Stop", "Uncontrolled", "Yield")
+#' @return Numeric value in mi/hr.
+#' @examples
+#' auto.S_R(link, control)
+#' @export
+auto.VCratio <- function(link, int) {
+  #Adjusted saturation flow rate
+  s = auto.satflow(link, int)
+  
+  #Lane group capacity
+  c = link$N_th*s*int[traf_dir == link$link_dir, g/C]
+  
+  #V/C Ratio
+  X = int[traf_dir == link$link_dir, v_v] / c
+  
+  return(X)
+}
+
+
+
+
